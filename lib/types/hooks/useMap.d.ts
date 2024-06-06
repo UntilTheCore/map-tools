3c52a004f59c20943fe22740e885dba74edea42b
utc <814898862@qq.com>
2024-06-06 17:22:22 +0800

    useMap return value export map instance.

--- lib/types/hooks/useMap.d.ts
+++ lib/types/hooks/useMap.d.ts
@@ -1,5 +1,5 @@
 /// <reference path="../minemap.d.ts" />
-import type { Feature, FeatureCollection } from '@turf/turf';
+import type { Feature, FeatureCollection } from "@turf/turf";
 export type EventDispatcherData = {
     layerId: string;
     feature: Feature | FeatureCollection;
@@ -24,7 +24,7 @@ export type MapHookOption = {
     bindClickLayers?: string[];
     bindMouseMoveLayers?: string[];
     bindZoomLayers?: string[];
-    zoomQueryBy?: 'map' | 'mouse';
+    zoomQueryBy?: "map" | "mouse";
 };
 /**
  * map hooks. 对各种类型的地图事件进行监听需要在对应的数组内进行绑定，同理，需要监听不在图层内的监听，则需要使用如 "noInLayer"的方法
@@ -37,14 +37,56 @@ export type MapHookOption = {
  * @returns
  */
 export declare function useMap(option: MapHookOption): {
+    mapInstance: import("vue").Ref<{
+        on: (eventName: string, callback: (e: any) => void) => void;
+        off: (eventName: string, callback: (e: any) => void) => void;
+        remove: () => void;
+        addSource: (id: string, option: MapSource) => void;
+        getSource: (id: string) => any;
+        removeSource: (id: string) => void;
+        addLayer: (option: MapLayer) => void;
+        getLayer: (id: string) => any;
+        removeLayer: (id: string) => void;
+        moveLayer: (downLayerId: string, upLayerId?: string | undefined) => void;
+        getZoom: () => number;
+        setZoom: (zoom: number) => void;
+        zoomTo: (zoom: number, options?: {
+            duration: number;
+            easing: () => number;
+            offset: number[];
+            animate: boolean;
+        } | undefined, eventData?: any) => void;
+        getCenter: () => {
+            lng: number;
+            lat: number;
+        };
+        setFilter: (layerId: string, condition: (string | number)[] | null) => void;
+        setLayoutProperty: (layerId: string, name: string, value: any, options?: any) => void;
+        panTo: (coordinate: number[]) => void;
+        queryRenderedFeatures: (point: number[], options?: {
+            layers?: string[] | undefined;
+            filter?: any[] | undefined;
+            validate?: boolean | undefined;
+        } | undefined) => any[];
+        loadImage: (url: string, cb: (error: any, image: any) => void) => void;
+        getCanvas: () => any;
+        hasImage: (url: any) => boolean;
+        addImage: (name: string, image: any, option?: any) => void;
+        triggerRepaint: () => void;
+        fitBounds: (bound: any, option: any) => void;
+        easeTo: (param: {
+            center: number[];
+            zoom: number;
+        }) => void;
+    } | null | undefined>;
     mapInitialize: (map: minemap.Map) => void;
     onMapLoaded: (fn: BindFn["onMapLoaded"]) => void;
-    onClickLayerEventDispatcher: (fn: BindFn['clickLayerEventDispatcher']) => void;
-    onClickNoInLayers: (fn: BindFn['clickNoInLayer']) => void;
-    onMouseMoveLayerEventDispatcher: (fn: BindFn['mouseMoveLayerEventDispatcher']) => void;
-    onMoveNoInLayers: (fn: BindFn['moveNoInLayer']) => void;
-    onZoomLayerEventDispatcher: (fn: BindFn['zoomLayerEventDispatcher']) => void;
-    onZoomNoInLayers: (fn: BindFn['zoomNoInLayer']) => void;
+    onClickLayerEventDispatcher: (fn: BindFn["clickLayerEventDispatcher"]) => void;
+    onClickNoInLayers: (fn: BindFn["clickNoInLayer"]) => void;
+    onMouseMoveLayerEventDispatcher: (fn: BindFn["mouseMoveLayerEventDispatcher"]) => void;
+    onMoveNoInLayers: (fn: BindFn["moveNoInLayer"]) => void;
+    onZoomLayerEventDispatcher: (fn: BindFn["zoomLayerEventDispatcher"]) => void;
+    onZoomNoInLayers: (fn: BindFn["zoomNoInLayer"]) => void;
     unBindMapEvent: () => void;
 };
 export {};
--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -1,36 +1,36 @@
-import {onUnmounted, ref, reactive} from "vue";
-import type { Feature, FeatureCollection } from '@turf/turf'
+import { onUnmounted, ref, reactive } from "vue";
+import type { Feature, FeatureCollection } from "@turf/turf";
 
 export type EventDispatcherData = {
-  layerId: string;
-  feature: Feature | FeatureCollection;
-  mapEvent: any;
-}
+    layerId: string;
+    feature: Feature | FeatureCollection;
+    mapEvent: any;
+};
 
 export type ZoomLayerEventData = {
-  zoom: number,
-  mouseCoordinate: number[],
-  mapCenterCoordinate: number[],
+    zoom: number;
+    mouseCoordinate: number[];
+    mapCenterCoordinate: number[];
 } & EventDispatcherData;
 
 type BindFn = {
-  onMapLoaded?: (map: minemap.Map) => void;
-  clickLayerEventDispatcher?: (data: EventDispatcherData) => void;
-  clickNoInLayer?: (e: any) => void;
-  mouseMoveLayerEventDispatcher?: (data: EventDispatcherData) => void;
-  moveNoInLayer?: (e: any) => void;
-  zoomLayerEventDispatcher?: (data: ZoomLayerEventData) => void;
-  zoomNoInLayer?: (e: any) => void;
-}
+    onMapLoaded?: (map: minemap.Map) => void;
+    clickLayerEventDispatcher?: (data: EventDispatcherData) => void;
+    clickNoInLayer?: (e: any) => void;
+    mouseMoveLayerEventDispatcher?: (data: EventDispatcherData) => void;
+    moveNoInLayer?: (e: any) => void;
+    zoomLayerEventDispatcher?: (data: ZoomLayerEventData) => void;
+    zoomNoInLayer?: (e: any) => void;
+};
 
 export type MapHookOption = {
-  map?: minemap.Map;
-  bindClickLayers?: string[],
-  bindMouseMoveLayers?: string[],
-  bindZoomLayers?: string[],
-  // onMapZoom 以哪个坐标为基准进行图层查询
-  zoomQueryBy?: 'map' | 'mouse',
-}
+    map?: minemap.Map;
+    bindClickLayers?: string[];
+    bindMouseMoveLayers?: string[];
+    bindZoomLayers?: string[];
+    // onMapZoom 以哪个坐标为基准进行图层查询
+    zoomQueryBy?: "map" | "mouse";
+};
 
 /**
  * map hooks. 对各种类型的地图事件进行监听需要在对应的数组内进行绑定，同理，需要监听不在图层内的监听，则需要使用如 "noInLayer"的方法
@@ -40,215 +40,227 @@ export type MapHookOption = {
  * @param [option.bindMouseMoveLayers] - 绑定地图内监听鼠标移动事件监听的图层。绑定后，会在 onMouseMoveLayerEventDispatcher 的回调参数中获取到点击的图层相关数据
  * @param [option.bindZoomLayers] - 绑定地图内监听缩放事件监听的图层。绑定后，会在 onZoomLayerEventDispatcher 的回调参数中获取到点击的图层相关数据
  * @param [option.zoomQueryBy] - onMapZoom 以哪个坐标为基准进行图层查询
- * @returns 
+ * @returns
  */
 export function useMap(option: MapHookOption) {
-  const {
-    map = null,
-    bindClickLayers = [],
-    bindMouseMoveLayers = [],
-    bindZoomLayers = [],
-    zoomQueryBy = 'mouse',
-  } = option;
-  const mapInstance = ref<minemap.Map | null | undefined>(map);
-  const mouseInfo = reactive({
-    lng: 0,
-    lat: 0,
-    x: 0,
-    y: 0,
-  })
+    const {
+        map = null,
+        bindClickLayers = [],
+        bindMouseMoveLayers = [],
+        bindZoomLayers = [],
+        zoomQueryBy = "mouse",
+    } = option;
+    const mapInstance = ref<minemap.Map | null | undefined>(map);
+    const mouseInfo = reactive({
+        lng: 0,
+        lat: 0,
+        x: 0,
+        y: 0,
+    });
 
-  if(mapInstance.value) {
-    bindMapEvent();
-  }
-
-  const bindFnList = reactive<BindFn>({
-    onMapLoaded: undefined,
-    clickLayerEventDispatcher: undefined,
-    clickNoInLayer: undefined,
-    mouseMoveLayerEventDispatcher: undefined,
-    moveNoInLayer: undefined,
-    zoomLayerEventDispatcher: undefined,
-    zoomNoInLayer: undefined,
-  })
-
-  /**
-   * 当地图的初始化是通过组件的事件返回时，可以通过绑定此函数执行初始化操作。
-   * 注意：当 useMap 默认设置了 map 实例后，为了防止重复绑定，此方法的执行将被忽略
-   * @param map
-   */
-  function mapInitialize(map: minemap.Map) {
-    if(!mapInstance.value) {
-      mapInstance.value = map;
-      bindFnList.onMapLoaded && bindFnList.onMapLoaded(map);
-      mapInstance.value.on("click", onMapClick);
-      mapInstance.value.on("mousemove", onMapMouseMove);
-      mapInstance.value.on("zoomend", onMapZoomEnd);
-    }
-  }
-
-  function bindMapEvent() {
-    if(mapInstance.value) {
-      mapInstance.value.on("click", onMapClick);
-      mapInstance.value.on("mousemove", onMapMouseMove);
-      mapInstance.value.on("zoomend", onMapZoomEnd);
-    }
-  }
-
-  function unBindMapEvent() {
-    if(mapInstance.value) {
-      mapInstance.value.off("click", onMapClick);
-      mapInstance.value.off("mousemove", onMapMouseMove);
-      mapInstance.value.off("zoomend", onMapZoomEnd);
-    }
-  }
-
-  function onMapZoomEnd(e: any) {
-    const zoom = mapInstance?.value!.getZoom();
-    const mapCenter = mapInstance?.value!.getCenter();
-    let queryPoint;
-    if(zoomQueryBy === 'mouse') {
-      queryPoint = [mouseInfo.x, mouseInfo.y];
-    } else if(zoomQueryBy === 'map') {
-      // 使用坐标查询会有偏移，慎重使用
-      queryPoint = [mapCenter.lng, mapCenter.lat];
+    if (mapInstance.value) {
+        bindMapEvent();
     }
 
-    if(!!queryPoint) {
-      const features = mapInstance?.value?.queryRenderedFeatures(queryPoint, {
-        layers: bindZoomLayers,
-      });
+    const bindFnList = reactive<BindFn>({
+        onMapLoaded: undefined,
+        clickLayerEventDispatcher: undefined,
+        clickNoInLayer: undefined,
+        mouseMoveLayerEventDispatcher: undefined,
+        moveNoInLayer: undefined,
+        zoomLayerEventDispatcher: undefined,
+        zoomNoInLayer: undefined,
+    });
 
-      if (Array.isArray(features) && features.length > 0) {
-        const feature = features[0];
-        const zoomLayerId = feature.layer.id;
-        if (!!feature && bindZoomLayers.includes(zoomLayerId)) {
-          bindFnList.zoomLayerEventDispatcher && bindFnList.zoomLayerEventDispatcher({
-            zoom,
-            layerId: zoomLayerId,
-            feature,
-            mapEvent: e,
-            mouseCoordinate: [mouseInfo.lng, mouseInfo.lat],
-            mapCenterCoordinate: [mapCenter.lng, mapCenter.lat],
-          });
+    /**
+     * 当地图的初始化是通过组件的事件返回时，可以通过绑定此函数执行初始化操作。
+     * 注意：当 useMap 默认设置了 map 实例后，为了防止重复绑定，此方法的执行将被忽略
+     * @param map
+     */
+    function mapInitialize(map: minemap.Map) {
+        if (!mapInstance.value) {
+            mapInstance.value = map;
+            bindFnList.onMapLoaded && bindFnList.onMapLoaded(map);
+            mapInstance.value.on("click", onMapClick);
+            mapInstance.value.on("mousemove", onMapMouseMove);
+            mapInstance.value.on("zoomend", onMapZoomEnd);
         }
-      } else {
-        bindFnList.zoomNoInLayer && bindFnList.zoomNoInLayer(e);
-      }
-
     }
-  }
 
-  function setMouseInfo(e: any) {
-    const { lng, lat } = e.lngLat;
-    const {x, y} = e.point;
-    mouseInfo.lng = lng;
-    mouseInfo.lat = lat;
-    mouseInfo.x = x;
-    mouseInfo.y = y;
-  }
+    function bindMapEvent() {
+        if (mapInstance.value) {
+            mapInstance.value.on("click", onMapClick);
+            mapInstance.value.on("mousemove", onMapMouseMove);
+            mapInstance.value.on("zoomend", onMapZoomEnd);
+        }
+    }
 
-  function onMapMouseMove(e: any) {
-    setMouseInfo(e);
+    function unBindMapEvent() {
+        if (mapInstance.value) {
+            mapInstance.value.off("click", onMapClick);
+            mapInstance.value.off("mousemove", onMapMouseMove);
+            mapInstance.value.off("zoomend", onMapZoomEnd);
+        }
+    }
 
-    const features = mapInstance?.value?.queryRenderedFeatures(e.point, {
-      layers: bindMouseMoveLayers,
+    function onMapZoomEnd(e: any) {
+        const zoom = mapInstance?.value!.getZoom();
+        const mapCenter = mapInstance?.value!.getCenter();
+        let queryPoint;
+        if (zoomQueryBy === "mouse") {
+            queryPoint = [mouseInfo.x, mouseInfo.y];
+        } else if (zoomQueryBy === "map") {
+            // 使用坐标查询会有偏移，慎重使用
+            queryPoint = [mapCenter.lng, mapCenter.lat];
+        }
+
+        if (!!queryPoint) {
+            const features = mapInstance?.value?.queryRenderedFeatures(
+                queryPoint,
+                {
+                    layers: bindZoomLayers,
+                }
+            );
+
+            if (Array.isArray(features) && features.length > 0) {
+                const feature = features[0];
+                const zoomLayerId = feature.layer.id;
+                if (!!feature && bindZoomLayers.includes(zoomLayerId)) {
+                    bindFnList.zoomLayerEventDispatcher &&
+                        bindFnList.zoomLayerEventDispatcher({
+                            zoom,
+                            layerId: zoomLayerId,
+                            feature,
+                            mapEvent: e,
+                            mouseCoordinate: [mouseInfo.lng, mouseInfo.lat],
+                            mapCenterCoordinate: [mapCenter.lng, mapCenter.lat],
+                        });
+                }
+            } else {
+                bindFnList.zoomNoInLayer && bindFnList.zoomNoInLayer(e);
+            }
+        }
+    }
+
+    function setMouseInfo(e: any) {
+        const { lng, lat } = e.lngLat;
+        const { x, y } = e.point;
+        mouseInfo.lng = lng;
+        mouseInfo.lat = lat;
+        mouseInfo.x = x;
+        mouseInfo.y = y;
+    }
+
+    function onMapMouseMove(e: any) {
+        setMouseInfo(e);
+
+        const features = mapInstance?.value?.queryRenderedFeatures(e.point, {
+            layers: bindMouseMoveLayers,
+        });
+
+        if (Array.isArray(features) && features.length > 0) {
+            const feature = features[0];
+            const moveLayerId = feature.layer.id;
+            if (!!feature && bindMouseMoveLayers.includes(moveLayerId)) {
+                bindFnList.mouseMoveLayerEventDispatcher &&
+                    bindFnList.mouseMoveLayerEventDispatcher({
+                        layerId: moveLayerId,
+                        feature,
+                        mapEvent: e,
+                    });
+            }
+        } else {
+            // 点击了非图层区域
+            bindFnList.moveNoInLayer && bindFnList.moveNoInLayer(e);
+        }
+    }
+
+    function onMapClick(e: any) {
+        const features = mapInstance?.value?.queryRenderedFeatures(e.point, {
+            layers: bindClickLayers || [],
+        });
+
+        if (Array.isArray(features) && features.length > 0) {
+            const feature = features[0];
+            const clickLayerId = feature.layer.id;
+            if (!!feature && bindClickLayers.includes(clickLayerId)) {
+                bindFnList.clickLayerEventDispatcher &&
+                    bindFnList.clickLayerEventDispatcher({
+                        layerId: clickLayerId,
+                        feature,
+                        mapEvent: e,
+                    });
+            }
+        } else {
+            // 点击了非图层区域
+            bindFnList.clickNoInLayer && bindFnList.clickNoInLayer(e);
+        }
+    }
+
+    function onMapLoaded(fn: BindFn["onMapLoaded"]) {
+        bindFnList.onMapLoaded = fn;
+    }
+
+    /**
+     * 被点击图层事件分发器
+     */
+    function onClickLayerEventDispatcher(
+        fn: BindFn["clickLayerEventDispatcher"]
+    ) {
+        bindFnList.clickLayerEventDispatcher = fn;
+    }
+
+    // 点击了非绑定图层区域
+    function onClickNoInLayers(fn: BindFn["clickNoInLayer"]) {
+        bindFnList.clickNoInLayer = fn;
+    }
+
+    /**
+     * 鼠标移动图层事件分发器
+     */
+    function onMouseMoveLayerEventDispatcher(
+        fn: BindFn["mouseMoveLayerEventDispatcher"]
+    ) {
+        bindFnList.mouseMoveLayerEventDispatcher = fn;
+    }
+
+    /**
+     * 移动到了非绑定图层区域
+     */
+    function onMoveNoInLayers(fn: BindFn["moveNoInLayer"]) {
+        bindFnList.moveNoInLayer = fn;
+    }
+
+    /**
+     * 鼠标移动图层事件分发器
+     */
+    function onZoomLayerEventDispatcher(
+        fn: BindFn["zoomLayerEventDispatcher"]
+    ) {
+        bindFnList.zoomLayerEventDispatcher = fn;
+    }
+
+    /**
+     * 移动到了非绑定图层区域
+     */
+    function onZoomNoInLayers(fn: BindFn["zoomNoInLayer"]) {
+        bindFnList.zoomNoInLayer = fn;
+    }
+
+    onUnmounted(() => {
+        unBindMapEvent();
     });
 
-    if (Array.isArray(features) && features.length > 0) {
-      const feature = features[0];
-      const moveLayerId = feature.layer.id;
-      if (!!feature && bindMouseMoveLayers.includes(moveLayerId)) {
-        bindFnList.mouseMoveLayerEventDispatcher && bindFnList.mouseMoveLayerEventDispatcher({
-          layerId: moveLayerId,
-          feature,
-          mapEvent: e,
-        });
-      }
-    } else {
-      // 点击了非图层区域
-      bindFnList.moveNoInLayer && bindFnList.moveNoInLayer(e);
-    }
-  }
-
-  function onMapClick(e: any) {
-    const features = mapInstance?.value?.queryRenderedFeatures(e.point, {
-      layers: bindClickLayers || [],
-    });
-
-    if (Array.isArray(features) && features.length > 0) {
-      const feature = features[0];
-      const clickLayerId = feature.layer.id
-      if (!!feature && bindClickLayers.includes(clickLayerId)) {
-        bindFnList.clickLayerEventDispatcher && bindFnList.clickLayerEventDispatcher({
-          layerId: clickLayerId,
-          feature,
-          mapEvent: e,
-        });
-      }
-    } else {
-      // 点击了非图层区域
-      bindFnList.clickNoInLayer && bindFnList.clickNoInLayer(e);
-    }
-  }
-
-  function onMapLoaded(fn: BindFn["onMapLoaded"]) {
-    bindFnList.onMapLoaded = fn;
-  }
-
-  /**
-   * 被点击图层事件分发器
-   */
-  function onClickLayerEventDispatcher(fn: BindFn['clickLayerEventDispatcher']) {
-    bindFnList.clickLayerEventDispatcher = fn;
-  }
-
-  // 点击了非绑定图层区域
-  function onClickNoInLayers(fn: BindFn['clickNoInLayer']) {
-    bindFnList.clickNoInLayer = fn;
-  }
-
-  /**
-   * 鼠标移动图层事件分发器
-   */
-  function onMouseMoveLayerEventDispatcher(fn: BindFn['mouseMoveLayerEventDispatcher']) {
-    bindFnList.mouseMoveLayerEventDispatcher = fn;
-  }
-
-  /**
-   * 移动到了非绑定图层区域
-   */
-  function onMoveNoInLayers(fn: BindFn['moveNoInLayer']) {
-    bindFnList.moveNoInLayer = fn;
-  }
-
-  /**
-   * 鼠标移动图层事件分发器
-   */
-  function onZoomLayerEventDispatcher(fn: BindFn['zoomLayerEventDispatcher']) {
-    bindFnList.zoomLayerEventDispatcher = fn;
-  }
-
-  /**
-   * 移动到了非绑定图层区域
-   */
-  function onZoomNoInLayers(fn: BindFn['zoomNoInLayer']) {
-    bindFnList.zoomNoInLayer = fn;
-  }
-
-  onUnmounted(() => {
-    unBindMapEvent();
-  })
-
-  return {
-    mapInitialize,
-    onMapLoaded,
-    onClickLayerEventDispatcher,
-    onClickNoInLayers,
-    onMouseMoveLayerEventDispatcher,
-    onMoveNoInLayers,
-    onZoomLayerEventDispatcher,
-    onZoomNoInLayers,
-    unBindMapEvent,
-  }
+    return {
+        mapInstance,
+        mapInitialize,
+        onMapLoaded,
+        onClickLayerEventDispatcher,
+        onClickNoInLayers,
+        onMouseMoveLayerEventDispatcher,
+        onMoveNoInLayers,
+        onZoomLayerEventDispatcher,
+        onZoomNoInLayers,
+        unBindMapEvent,
+    };
 }
